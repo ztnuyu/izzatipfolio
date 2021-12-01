883a99b1f5f3127e27cd27658ba4c4a0c849adc6
ztnuyu <izzatisuhaimi@icloud.com>
2021-12-01 12:58:53 +0800

    next commit

--- src/App.vue
+++ src/App.vue
@@ -29,6 +29,9 @@
                   <a class="nav-link" href="#contact">Contact</a>
               </li>
               <li class="nav-item">
+                  <a class="nav-link" href="#contact">Test</a>
+              </li>
+              <li class="nav-item">
                   <a class="nav-link" href="#academic">Academic History</a>
               </li>
           </ul>
